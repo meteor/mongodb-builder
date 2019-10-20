a3c378260cad835711957ada766750bbb9a7010f
Christian Klaussner <christian@klaussner.me>
2019-10-20 12:53:27 +0200

    Update MongoDB to 4.2.1

--- run-builder.sh
+++ run-builder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-MONGODB_VERSION='4.2.0'
+MONGODB_VERSION='4.2.1'
 SRC="mongodb-src-r$MONGODB_VERSION"
 [ ! -d $SRC ] && curl "https://fastdl.mongodb.org/src/$SRC.tar.gz" | tar -xz
 docker run --rm -it -v $(pwd)/$SRC:/mongodb mongodb-builder
