b69f9799a293ce74a4eb02727d969d2718e7796e
Christian Klaussner <christian@klaussner.me>
2019-12-29 17:55:55 +0100

    Create archive after build is finished

--- run-builder.sh
+++ run-builder.sh
@@ -3,5 +3,11 @@ set -e
 
 MONGODB_VERSION='4.2.1'
 SRC="mongodb-src-r$MONGODB_VERSION"
+TARGET="mongodb-linux-x86_64-${MONGODB_VERSION}"
+BIN="$TARGET/bin"
+
 [ ! -d $SRC ] && curl "https://fastdl.mongodb.org/src/$SRC.tar.gz" | tar -xz
 docker run --rm -it -v $(pwd)/$SRC:/mongodb mongodb-builder
+mkdir -p $BIN
+mv "$SRC/mongo" "$SRC/mongod" $BIN
+tar -czf "$TARGET.tgz" $TARGET
