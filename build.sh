d7e09ed320eb647750101db7e52988fb01b912ac
Christian Klaussner <christian@klaussner.me>
2019-12-29 18:03:24 +0100

    Link `libstdc++` statically

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 cd /mongodb
 pip3 install -r etc/pip/compile-requirements.txt
-./buildscripts/scons.py mongod mongo --ssl=off --enable-free-mon=off
+./buildscripts/scons.py mongod mongo --ssl=off --enable-free-mon=off LINKFLAGS='-static-libstdc++'
 strip mongo mongod
