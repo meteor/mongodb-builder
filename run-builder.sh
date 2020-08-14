58c5834c898d11b56534488bbbcdd863ce486f2b
Renan Castro <renanccastro@gmail.com>
2020-08-14 10:19:21 -0300

    Use more memory when running docker to avoid compilation errors

--- run-builder.sh
+++ run-builder.sh
@@ -7,7 +7,7 @@ TARGET="mongodb-linux-x86_64-${MONGODB_VERSION}"
 BIN="$TARGET/bin"
 
 [ ! -d $SRC ] && curl "https://fastdl.mongodb.org/src/$SRC.tar.gz" | tar -xz
-docker run --rm -it -v $(pwd)/$SRC:/mongodb mongodb-builder
+docker run --memory=16g --rm -it -v $(pwd)/$SRC:/mongodb mongodb-builder
 mkdir -p $BIN
 mv "$SRC/mongo" "$SRC/mongod" $BIN
 tar -czf "$TARGET.tgz" $TARGET
